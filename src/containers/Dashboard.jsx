79e0ab88e48cf10499f63f31f451293dab10883d
Konstantsin Demedenkov <inbox7777777@gmail.com>
2019-09-21 12:16:03 +0300

    Decrease width to fix horizontal scrolling

--- src/containers/Dashboard.jsx
+++ src/containers/Dashboard.jsx
@@ -129,7 +129,7 @@ class Dashboard extends React.Component {
                     />
                 </div>
                 <div className="members">
-                    <MemberTable numberWidth="48"
+                    <MemberTable numberWidth="45"
                                  members={this.props.members}
                                  issues={this.props.issues}
                                  issuesSpentTime={this.props.issuesSpentTime}
